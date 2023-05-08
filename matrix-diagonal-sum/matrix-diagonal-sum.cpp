b1274593e9b247b326483e115b692e34fda55071
nandansrikrishna <97712595+nandansrikrishna@users.noreply.github.com>
2023-05-07 22:46:17 -0400

    Time: 19 ms (17.77%) | Memory: 11.2 MB (64.67%) - LeetSync

--- matrix-diagonal-sum/matrix-diagonal-sum.cpp
+++ matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -4,22 +4,12 @@ public:
         int size = mat.size();
         int sum = 0;
 
-        // odd
-        if (size % 2) {
-            for (int i = 0; i < size; ++i) {
-                sum += mat[i][i];
-                if (size - 1 - i != i) {
-                    sum += mat[size - 1 - i][i];
-                }
-            } 
-        }
-        // even
-        else {
-            for (int i = 0; i < size; ++i) {
-                sum += mat[i][i];
+        for (int i = 0; i < size; ++i) {
+            sum += mat[i][i];
+            if (size - 1 - i != i) {
                 sum += mat[size - 1 - i][i];
-            } 
-        }
+            }
+        } 
 
         return sum;
     }
