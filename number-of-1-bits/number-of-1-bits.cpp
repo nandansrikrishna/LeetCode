eda193816cd290cd67dfd2509bfdd8bf618abeee
nandansrikrishna <97712595+nandansrikrishna@users.noreply.github.com>
2023-05-26 18:17:18 -0400

    Time: 0 ms (100.00%) | Memory: 6 MB (74.45%) - LeetSync

--- number-of-1-bits/number-of-1-bits.cpp
+++ number-of-1-bits/number-of-1-bits.cpp
@@ -3,9 +3,9 @@ public:
     int hammingWeight(uint32_t n) {
         int count = 0;
 
-        while (n != 0) {
-            count += n%2;
-            n /= 2;
+        while (n) {
+            n &= (n - 1);
+            ++count;
         }
 
         return count;
