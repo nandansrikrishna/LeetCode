b95277210be601bf1cb1dd379a2b48a0aada818d
nandansrikrishna <97712595+nandansrikrishna@users.noreply.github.com>
2023-05-27 00:23:33 -0400

    Time: 75 ms (99.67%) | Memory: 67.6 MB (89.46%) - LeetSync

--- maximum-subarray/maximum-subarray.cpp
+++ maximum-subarray/maximum-subarray.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+        
         int currMax = 0, maxTillNow = INT_MIN;
         for (auto num : nums) {
             currMax = max(num, currMax + num);
