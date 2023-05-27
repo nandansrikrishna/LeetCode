bc1f3245fbc057b5b5eb65e0c9f73a148b05cd43
nandansrikrishna <97712595+nandansrikrishna@users.noreply.github.com>
2023-05-27 19:15:53 -0400

    Time: 11 ms (84.91%) | Memory: 5.9 MB (52.54%) - LeetSync

--- palindrome-number/palindrome-number.cpp
+++ palindrome-number/palindrome-number.cpp
@@ -2,19 +2,16 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         // Reconstruct number from right side of original pushing to left
-
         if (x < 0 || (x > 0 && x%10 == 0)) {
             return false;
         }
-        
-        int num = x;
-        long reverse = 0;
+        int reverse = 0;
 
-        while (num != 0) {
-            reverse = (reverse * 10) + (num%10);
-            num /= 10;
+        while (x > reverse) {
+            reverse = (reverse * 10) + (x%10);
+            x /= 10;
         }
 
-        return x == reverse;
+        return x == reverse || x == reverse/10;
     }
 };
