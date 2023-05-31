e36471f25ee351594c525c98bc0868f397d4f789
nandansrikrishna <97712595+nandansrikrishna@users.noreply.github.com>
2023-05-31 01:26:19 -0400

    Time: 21 ms (59.67%) | Memory: 16.9 MB (76.58%) - LeetSync

--- single-number/single-number.cpp
+++ single-number/single-number.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int bit = 0;
-        for (auto& num : nums) {
-            bit ^= num;
+        int ans = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            ans ^= nums[i];
         }
-        return bit;
+        return ans;
     }
 };
