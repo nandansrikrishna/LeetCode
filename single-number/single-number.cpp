b33484f933b9342531815ace7d8aa883111b63d5
nandansrikrishna <97712595+nandansrikrishna@users.noreply.github.com>
2023-05-31 01:28:02 -0400

    Time: 20 ms (70.04%) | Memory: 17.1 MB (22.66%) - LeetSync

--- single-number/single-number.cpp
+++ single-number/single-number.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         int ans = 0;
         for (auto& num : nums) {
             ans ^= num;
